130ae01a0ac4826422e3d7f11d0d4ed1761b60e9
Jens Axel Søgaard <jensaxel@soegaard.net>
2021-09-02 15:10:51 +0200

    Update info file for docs

--- racket-cas-doc/info.rkt
+++ racket-cas-doc/info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection "racket-cas-doc")
-(define deps '("racket"))
-(define build-deps '("racket-doc" "scribble-lib"))
+(define deps       '("base" "racket" "math-lib" "scribble-lib" "rackunit-lib"))
+(define build-deps '("base" "racket" "math-lib" "scribble-lib" "rackunit-lib" "racket-doc" "at-exp-lib"))
 
 
 
