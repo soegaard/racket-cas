82433eb4c3f3295271e3320042ecdac658d06a0f
Jens Axel Søgaard <jensaxel@soegaard.net>
2021-09-02 15:07:57 +0200

    Update deps in the hopefully correct info file

--- racket-cas/info.rkt
+++ racket-cas/info.rkt
@@ -1,6 +1,7 @@
 #lang info
 (define collection "racket-cas")
-(define build-deps '("rackunit-lib"))
+(define deps       '("base" "racket" "math-lib" "scribble-lib" "rackunit-lib"))
+(define build-deps '("base" "racket" "math-lib" "scribble-lib" "rackunit-lib" "racket-doc"))
 
 
 
