dfa875554249fdac6c48b4d292dfc4da93e1f1fd
Jens Axel Søgaard <jensaxel@soegaard.net>
2021-09-02 15:37:17 +0200

    Commit info file

--- racket-cas/info.rkt
+++ racket-cas/info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection "racket-cas")
 (define deps       '("base" "racket" "math-lib" "scribble-lib" "rackunit-lib"))
-(define build-deps '("base" "racket" "math-lib" "scribble-lib" "rackunit-lib" "racket-doc"))
+(define build-deps '("base" "racket" "math-lib" "scribble-lib" "rackunit-lib" "racket-doc" "at-exp-lib"))
 
 
 
