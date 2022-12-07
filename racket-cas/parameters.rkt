04e36cbd3f702edd443f5b4a6b773130b9a13786
Jens Axel Søgaard <jensaxel@soegaard.net>
2022-12-07 18:16:31 +0100

    Add the parameter default-output-implicit-product?

--- racket-cas/parameters.rkt
+++ racket-cas/parameters.rkt
@@ -1,5 +1,7 @@
 #lang racket/base
 
-(provide current-simplify)
+(provide current-simplify default-output-implicit-product?)
 
 (define current-simplify  (make-parameter (λ (u) u))) ; set to simplify  in "simplify-expand.rkt"
+
+(define default-output-implicit-product? (make-parameter #t))
